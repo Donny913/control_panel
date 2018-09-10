3e6ada30ce20bf59a98f76aa5ab20e4c4459b6bd
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-09-10 14:03:03 +0300

    playing with backgrounds

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { ThemeProvider, withStyles, Arwes, Button } from 'arwes';
 import basicStylesTheme from './theme';
 
+const background = 'https://i.pinimg.com/originals/d7/8d/40/d78d4069da54ade6085b7d540cfde597.jpg';
+// const background =
+
 const CustomText = withStyles(theme => ({
   customText: { color: theme.customColors.main }
 }))(props => <div className={props.classes.customText}>wow</div>);
@@ -10,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={basicStylesTheme}>
-        <Arwes>
+        <Arwes background={background}>
           <Button>Click me</Button>
           <CustomText />
         </Arwes>
